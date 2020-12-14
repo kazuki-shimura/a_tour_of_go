a3cea1e6b94d289c0bb912addc74590418cb755a
kazuki-shimura <sim202025@icloud.com>
2020-12-14 23:48:22 +0900

    変数

--- PVF/variables.go
+++ PVF/variables.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt" 
+	"math"
 )
 
 //No.8		変数の使用
@@ -43,6 +44,13 @@ import (
 // }
 
 
+// No.13	型変換
+func main() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+}
 
 
 
