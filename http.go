c244e167779e2d7c1d798fce9749d74f26ec87d2
kazuki-shimura <sim202025@icloud.com>
2020-12-16 23:50:49 +0900

    domツリーの挿入

--- http.go
+++ http.go
@@ -9,9 +9,13 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello HTTPサーバー")
 }
+func handler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<div><h1>田中</h1></div>")
+}
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/2", handler2)
 	http.ListenAndServe(":8080", nil)
 }
 
